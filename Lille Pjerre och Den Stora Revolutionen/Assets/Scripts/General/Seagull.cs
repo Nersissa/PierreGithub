186bfe21b58d6fb254feed135abd495048605791
kossanrosa <karlemil.svensson@gmail.com>
2015-04-19 13:52:58 +0200

    Pillat lite mer med måsar
    Och sånt

--- Lille Pjerre och Den Stora Revolutionen/Assets/Scripts/General/Seagull.cs
+++ Lille Pjerre och Den Stora Revolutionen/Assets/Scripts/General/Seagull.cs
@@ -1,15 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Seagull : MonoBehaviour {
+public class Seagull : MonoBehaviour
+{
+    public int maxSpeed = 10;
+    public int minSpeed = 5;
+    public bool randomYPos = true;
+    public int maxRandYPos = 3;
+    public int minRandYPos = -2;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    float speed = 0.01f;
+    //float originalYPosition;
+
+    void Start()
+    {
+        //originalYPosition = transform.position.y;
+
+        int facing = Random.Range(1, 3);
+
+        FindNewSpeed();
+
+        if (facing >= 2)
+        {
+            transform.Rotate(0, 180, 0);
+            speed *= -1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //AmIOutOfBounds();
+        Fly();
+    }
+
+    private void Fly()
+    {
+        transform.position += new Vector3(speed, 0);
+    }
+    private void AmIOutOfBounds()
+    {
+        //If I am outside camera view, find a new position to appear at.
+
+        if (transform.position.x <= Camera.main.transform.position.x)
+        {
+            int newPosition = Random.Range(1, 2);
+
+            //Just change direction and find new Y-position
+
+            if (newPosition == 1)
+            {
+                transform.Rotate(0, 0, 0);
+                FindNewSpeed();
+            }
+
+            //Appear at other edge with random Y-position
+
+            else
+            {
+                transform.position = new Vector3(transform.position.x, Random.Range(0, (int)Camera.main.transform.position.y));
+                FindNewSpeed();
+            }
+        }
+    }
+
+    //Find a new speed between 0.01 and 0.005
+    private void FindNewSpeed()
+    {
+        speed = Random.Range(minSpeed, maxSpeed) / 1000f;
+    }
+    private void RandomYPosition()
+    {
+        if (randomYPos)
+            transform.position = new Vector3(transform.position.x, transform.position.y + Random.Range(minRandYPos, maxRandYPos));
+    }
 }
