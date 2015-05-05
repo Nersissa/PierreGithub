e829eea440c6b035916cef478325a63febe24147
Simon Berggren <simon.berggren581@gmail.com>
2015-05-05 12:08:56 +0200

    DisNewScene

---
+++ Lille Pjerre och Den Stora Revolutionen/Assets/Scripts/WalkToTownScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkToTownScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+public class WalkToTownQuest : Quest
+{
+    public QuestStep GetToJacques, TalkToJacques, GetToTown;
+
+    public WalkToTownQuest()
+        : base("Walk to town")
+    {
+
+    }
+
+
+}
